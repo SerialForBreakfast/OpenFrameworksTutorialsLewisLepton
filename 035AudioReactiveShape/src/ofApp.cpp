40e35262f736a0e54f4f210f6fd6708e5e92225e
Joe <joe@showblender.com>
2016-02-08 09:40:51 -0800

    Updated 035, 038

---
+++ 035AudioReactiveShape/src/ofApp.cpp
@@ -0,0 +1,97 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    beat.loadSound("beat.wav");
+    fftSmooth = new float [8192];
+    for (int i =0; i<8192; i++) {
+        fftSmooth[i] =0;
+    }
+    bands = 64;
+    beat.setLoop(true);
+    beat.setVolume(.3);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    ofSoundUpdate();
+    float * value = ofSoundGetSpectrum(bands);
+    for (int i = 0; i<bands; i++){
+        fftSmooth[i] *= 0.2f;
+        if (fftSmooth[i] < value[i]){
+            fftSmooth[i] = value[i];
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetColor(255);
+    for (int i = 0; i< bands; i++){
+        ofCircle(ofGetWidth()/2, ofGetHeight()/2, -(fftSmooth[i]*150));
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key){
+            case '1':
+            beat.play();
+
+            break;
+            case '2':
+            beat.stop();
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
